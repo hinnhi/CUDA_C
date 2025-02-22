e1975f314d9e06b139572ed95f39fd62d0188166
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2025-02-22 01:25:07 +0100

    Create 01-hello-gpu.cu

---
+++ 01/01-hello-gpu.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void helloCPU()
+{
+  printf("Hello from the CPU.\n");
+}
+
+// __global__ keyword indicates that the following function will run on the GPU
+__global__ void helloGPU()    
+{
+  printf("Hello also from the CPU.\n");
+}
+
+int main()
+{
+  helloCPU();
+
+  helloGPU<<<1,1>>>();    // 1 block of 1 tread
+  cudaDeviceSynchronize();    // will cause the host code (CPU) to wait until the device code (GPU) completes
+}
