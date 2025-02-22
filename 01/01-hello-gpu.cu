5237f962014d48b388a61de1016db77de45c1cae
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2025-02-22 14:36:07 +0100

    Update 01-hello-gpu.cu

--- 01/01-hello-gpu.cu
+++ 01/01-hello-gpu.cu
@@ -15,6 +15,6 @@ int main()
 {
   helloCPU();
 
-  helloGPU<<<1,1>>>();    // 1 block of 1 tread
+  helloGPU<<<1,1>>>();    // 1 block of 1 thread
   cudaDeviceSynchronize();    // will cause the host code (CPU) to wait until the device code (GPU) completes
 }
